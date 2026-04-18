29feae44bc053cb97779b60c4fb75fa34cad8226
chsiao <chsiao3@my.centennialcollege.ca>
2026-04-18 19:36:37 -0400

    feat: init project

---
+++ src/main/java/com/gagent/GagentApplication.java
@@ -0,0 +1,13 @@
+package com.gagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GagentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GagentApplication.class, args);
+    }
+
+}
